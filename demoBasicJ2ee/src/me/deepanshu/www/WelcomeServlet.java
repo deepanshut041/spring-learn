65d370dd6b28d739e192228ca16f05e7e0f63b2e
deepanshut041 <deepanshut041@gmail.com>
2017-10-06 16:01:55 +0530

    intaliized spring and covred basic work

---
+++ demoBasicJ2ee/src/me/deepanshu/www/WelcomeServlet.java
@@ -0,0 +1,47 @@
+package me.deepanshu.www;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+@WebServlet("/WelcomeServlet")
+public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WelcomeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String welcomeMessage[] = {"Hello", "Welcome to spring course"};
+		request.setAttribute("myWelcomeMessage", welcomeMessage);
+		
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("welcome.jsp");
+		
+		requestDispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
